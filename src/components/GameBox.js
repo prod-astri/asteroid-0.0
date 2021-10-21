c4ba461b563dc5603b23539905db07d23adcae31
prod.astri <58519454+prod-astri@users.noreply.github.com>
2021-10-21 13:13:33 +0200

    add squares heightmap

--- src/components/GameBox.js
+++ src/components/GameBox.js
@@ -1,12 +1,52 @@
 import Sketch from 'react-p5'
 
 export default function GameBox(){
-    const heightMatrix = [
-        [ 1, 15, -34, 65, 81 ],
-        [ 13, -17, -55, 56, 97 ],
-        [ -16, -1, 19, 95, 116 ],
-        [ -1, 56, 1, 47, 70 ],
-        [ 89, 67, -11, 17, 79 ]
+    const heightMatrix = 
+    [
+      [
+          9,  18,  -7, -44,
+        -34, -42, -28,  -5,
+         36
+      ],
+      [
+         10,  37,  20, -30,
+        -64, -13, -19,  36,
+         43
+      ],
+      [
+         55,  58, 48, -35,
+        -74, -44,  5,  49,
+         50
+      ],
+      [
+         89,  85,  30, -24,
+        -60, -39, -27,   1,
+         32
+      ],
+      [
+        105,  47,  39, -24,
+        -48, -60, -25, -32,
+        -14
+      ],
+      [
+        101,  79,  34,   7,
+        -59, -44, -43, -17,
+        -11
+      ],
+      [
+         92, 45,  43,  11,
+        -38, -9, -25, -18,
+         34
+      ],
+      [
+        45, 32,  0,  4, -11,
+        18,  2, 31, 50
+      ],
+      [
+          7, -16, -54, -28,
+        -20,  29,  77,  58,
+         87
+      ]
     ]
     let matrixUnit = 30;
     
@@ -20,7 +60,7 @@ export default function GameBox(){
         let y = 0;
         for (let line of heightMatrix){
             for (let heightValue of line){
-                p5.fill(heightValue * 2.55)
+                p5.fill(assignColor(heightValue))
                 p5.square(x, y, matrixUnit)
                 x += matrixUnit;
             }
@@ -30,7 +70,16 @@ export default function GameBox(){
     }
     
     function assignColor(heightValue){
-        
+        if (heightValue > 80) return 'white'
+        if (heightValue > 60) return 'orange'
+        if (heightValue > 40) return 'beige'
+        if (heightValue > 20) return 'lightgreen'
+        if (heightValue > 0) return'lightyellow'
+        if (heightValue > -20) return 'aliceblue'
+        if (heightValue > -40) return 'cyan'
+        if (heightValue > -60) return 'darkcyan'
+        if (heightValue > -80) return 'darkblue'
+        return 'black'
     }
 
     return <Sketch setup={setup} draw={draw} />
