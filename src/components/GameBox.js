0b53762893019d0b4e2c4c7a054ee4d37745496d
prod.astri <58519454+prod-astri@users.noreply.github.com>
2021-10-21 13:00:03 +0200

    add map displayer from heights matrix

--- src/components/GameBox.js
+++ src/components/GameBox.js
@@ -1,16 +1,38 @@
 import Sketch from 'react-p5'
 
 export default function GameBox(){
+    const heightMatrix = [
+        [ 1, 15, -34, 65, 81 ],
+        [ 13, -17, -55, 56, 97 ],
+        [ -16, -1, 19, 95, 116 ],
+        [ -1, 56, 1, 47, 70 ],
+        [ 89, 67, -11, 17, 79 ]
+    ]
+    let matrixUnit = 30;
+    
     const setup = (p5, canvasParentRef) => {
-        p5.createCanvas(500, 400).parent(canvasParentRef)
-      }
-      
-      const draw = p5 => {
-        p5.background(255, 130, 20)
-        p5.ellipse(100, 100, 100)
-        p5.ellipse(300, 100, 100)
-      }
-      
-      return <Sketch setup={setup} draw={draw} />
+        p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef)
     }
+    
+    const draw = p5 => {
+        p5.background(220, 220, 220)
+        let x = 0;
+        let y = 0;
+        for (let line of heightMatrix){
+            for (let heightValue of line){
+                p5.fill(heightValue * 2.55)
+                p5.square(x, y, matrixUnit)
+                x += matrixUnit;
+            }
+            y += matrixUnit
+            x = 0;
+        }
+    }
+    
+    function assignColor(heightValue){
+        
+    }
+
+    return <Sketch setup={setup} draw={draw} />
+}
 
