435d75ac0b10e396ed39b57661b6897bef031b67
prod.astri <58519454+prod-astri@users.noreply.github.com>
2021-10-21 12:00:28 +0200

    isolate GameBox component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 
 import React from 'react'
-import Sketch from 'react-p5'
+import GameBox from './components/GameBox'
 
 function App() {
-  const setup = (p5, canvasParentRef) => {
-    p5.createCanvas(500, 400).parent(canvasParentRef)
-  }
-  
-  const draw = p5 => {
-    p5.background(255, 130, 20)
-    p5.ellipse(100, 100, 100)
-    p5.ellipse(300, 100, 100)
-  }
-  
-  return <Sketch setup={setup} draw={draw} />
+  return (
+  <div>
+  <GameBox />
+  </div>)
 }
 
 export default App
